305795810ccd6ffb62982292f93acd01d624a168
makanaki <ibrokings@gmail.comgit config --global color.ui autogit configgit config --global user.email ibrokings@gmail.com>
2022-04-14 10:41:16 +0100

    payment

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,11 @@
 /** @format */
 const express = require("express");
-const { createUser, loginUser } = require("../controllers/user.controller");
+const { createUser, loginUser, payment } = require("../controllers/user.controller");
 
 const router = express.Router();
 
 router.post("/createUser", createUser); // signup new user
 router.post("/loginUser", loginUser); // new user login
+router.post("/auth/payment", payment);
 
 module.exports = router;
